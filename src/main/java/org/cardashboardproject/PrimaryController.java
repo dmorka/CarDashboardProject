0b924cdfd7be32dd5be3c7ffc31293a12909e0f7
dmorka <morkadawid@gmail.com>
2020-05-01 15:37:16 +0200

    About pop-up

--- src/main/java/org/cardashboardproject/PrimaryController.java
+++ src/main/java/org/cardashboardproject/PrimaryController.java
@@ -2,11 +2,21 @@ package org.cardashboardproject;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void openAbout() throws IOException {
+        Parent root = new FXMLLoader(App.class.getResource("about.fxml")).load();
+        Scene scene = new Scene(root);
+
+        Stage stage = new Stage();
+        stage.setTitle("About");
+        stage.setScene(scene);
+        stage.show();
     }
 }
